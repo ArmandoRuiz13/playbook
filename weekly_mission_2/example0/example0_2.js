3908b9666d679e187339138f07d32ad383d16980
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-19 19:24:00 -0500

    1. Objetos ejemplos

---
+++ weekly_mission_2/example0/example0_2.js
@@ -0,0 +1,10 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc = {
+    name: "Armando",
+    age: 22,
+    mission: "learn node js"
+} // Esto es un objeto vacío
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObjetc)
+console.log("Accediendo a las propiedades de objeto interno")
+console.log(myObjetc.name)
