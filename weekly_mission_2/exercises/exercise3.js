da3b38510e18702fec28ae0675664d1363408e96
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-21 17:41:29 -0500

    agregando la primera clase

---
+++ weekly_mission_2/exercises/exercise3.js
@@ -0,0 +1,18 @@
+class repo{
+    constructor(name,author,language,numberOfCommits,stars,forks,issues_open,issues_close){//getTotalIssues,getGeneralInfo){
+        this.name = name
+        this.author = author
+        this.language = language
+        this.numberOfCommits = numberOfCommits
+        this.stars = stars
+        this.forks = forks
+        this.issues_open = issues_open
+        this.issues_close = issues_close
+        // this.getTotalIssues = 
+        // this.getGeneralInfo
+    }
+}
+
+
+const repo1= new repo( "LaunchX","carlogilmar","JavaScript",100,199,299,10,10)
+console.log(repo1)
